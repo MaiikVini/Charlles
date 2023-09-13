a97c9ea5e0ea186ffc5c2fd8922ff3bccc12a517
MaiikVini <118492495+MaiikVini@users.noreply.github.com>
2023-09-13 12:30:03 -0300

    Delete util directory

--- util/M.java
+++
@@ -1,9 +0,0 @@
-package com.xk72.util;
-
-public interface m extends Runnable {}
-
-
-/* Location:              C:\Program Files\Charles\lib\charles.jar!\com\xk7\\util\m.class
- * Java compiler version: 11 (55.0)
- * JD-Core Version:       1.1.3
- */
--- util/css/Xsnk.java
+++
@@ -1,7 +0,0 @@
-package com.xk72.util.css;
-
-
-/* Location:              C:\Program Files\Charles\lib\charles.jar!\com\xk7\\util\css\Xsnk.class
- * Java compiler version: 11 (55.0)
- * JD-Core Version:       1.1.3
- */
